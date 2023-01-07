62ce9d695323cf8d5279cd7b473d7b7ab51b3f7a
Goosa2025 <gus21002@byui.edu>
2023-01-06 17:53:31 -0700

    Initial setup

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,16 @@
 const routes = require('express').Router();
+const testLink = '/CSE341ag/testpage/new.html'
 
-routes.get('/', (req, res) => {
-    res.send('Hello Amber and Ames and Alice and Art and Adam!')
-  });
+routes.get('/', (req, res, next) => {
+    res.send('Hello Amber and Ames and Alice and Art and Adam!');
+});
+
+routes.get('/test', (req, res, next) => {
+  res.send('Hello there');
+});
+
+
+
 
 //export - in this class this is the method to export routes in any node API
 
